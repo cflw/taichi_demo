5045077ddf485e0b1e3f51bef95e8e3c0978b8f3
cflw <cflw1994@msn.cn>
2021-11-16 07:32:16 +0800

    20211116

--- 光线追踪/公共.py
+++ 光线追踪/公共.py
@@ -1,5 +1,7 @@
 import taichi as ti
 import enum
+from PIL import Image	#pillow
+import numpy as np	#numpy
 #常量
 PI = 3.14159265
 半PI = 1.5707963
@@ -7,10 +9,15 @@ PI = 3.14159265
 c最小值 = 0.0001
 c最大值 = 10e5
 #类型
+t向量2 = ti.types.vector(2, float)
 t向量3 = ti.types.vector(3, float)
+t向量4 = ti.types.vector(4, float)
 #===============================================================================
 # 函数
 #===============================================================================
+@ti.func
+def lerp(a, b, t):
+	return a + (b - a) * t
 @ti.func
 def random_vector3():	#随机向量3
 	return t向量3(ti.random(), ti.random(), ti.random())
@@ -53,23 +60,140 @@ class C球体:
 		self.m颜色 = a颜色	#物体反射光
 		self.m材质 = a材质
 	@ti.func
-	def intersect(self, a光线, a最小值, a最大值):	#求光线交于圆表面的距离,不相交则返回0
+	def hit(self, a光线, a最小值, a最大值):	#求光线交于圆表面的距离,不相交则返回0
 		v相对位置 = a光线.m位置 - self.m位置
 		a = a光线.m方向.dot(a光线.m方向)
 		b = 2 * v相对位置.dot(a光线.m方向)
 		c = v相对位置.dot(v相对位置) - self.m半径 * self.m半径
 		d = b * b - 4 * a * c
 		x = 0.0
-		if d > 0:
+		if d > 0.0:
 			sqrt_d = ti.sqrt(d)
 			x = (-b - sqrt_d) / (2 * a)
 			if x < a最小值 or x > a最大值:
 				x = (-b + sqrt_d) / (2 * a)
 				if x < a最小值 or x > a最大值:
-					x = 0
+					x = 0.0
 		else:
-			x = 0
-		return x
+			x = 0.0
+		#返回
+		v碰撞 = False
+		v交点 = t向量3(0, 0, 0)
+		v交点法线 = t向量3(0, 0, 0)
+		v前面 = False
+		if x > 0.0:
+			v碰撞 = True
+			v交点 = a光线.at(x)
+			v交点法线 = (v交点 - self.m位置).normalized()
+			if a光线.m方向.dot(v交点法线) < 0:
+				v前面 = True
+			else:
+				v交点法线 = -v交点法线
+		return v碰撞, x, v交点, v交点法线, v前面, self.m颜色, self.m材质
+@ti.data_oriented
+class C矩形:	#平面的
+	def __init__(self, a位置: t向量3, a尺寸: t向量2, a旋转: float, a颜色: t向量3, a材质):
+		self.m位置 = a位置
+		self.m半尺寸 = a尺寸 * 0.5
+		self.m旋转 = a旋转
+		self.m颜色 = a颜色
+		self.m材质 = a材质
+	@ti.func
+	def hit(self, a光线, a最小值, a最大值):
+		#旋转世界,把图片放置于y-z平面
+		c = ti.cos(-self.m旋转)
+		s = ti.sin(-self.m旋转)
+		v旋转矩阵 = ti.Matrix([[c, 0.0, s], [0.0, 1.0, 0.0], [-s, 0.0, c]])
+		v相对位置 = a光线.m位置 - self.m位置
+		v相对位置 = v旋转矩阵 @ v相对位置
+		v相对方向 = v旋转矩阵 @ a光线.m方向
+		#计算相交
+		v碰撞 = False
+		t = 0.0
+		v交点 = t向量3(0.0, 0.0, 0.0)
+		v交点法线 = t向量3(0.0, 0.0, 0.0)
+		v前面 = False
+		#有2种情况:
+		#1:相对位置是负,为了相交,相对方向应该是正的
+		#2:相对位置是正,为了相交,相对方向应该是负的
+		t = -v相对位置.x / v相对方向.x	#如果存在相交的情况,t应该是正的
+		v相对交点 = v相对位置 + v相对方向 * t
+		if t < a最小值 or t > a最大值:
+			pass
+		elif abs(v相对交点.z) < self.m半尺寸.x and abs(v相对交点.y) < self.m半尺寸.y:	#交点在矩形内
+			v碰撞 = True
+			v交点 = a光线.at(t)
+			v交点法线 = t向量3(c, 0.0, s)
+			if v相对位置.x >= 0:
+				v前面 = True
+			else:
+				v交点法线 = -v交点法线
+		return v碰撞, t, v交点, v交点法线, v前面, self.m颜色, self.m材质
+@ti.data_oriented
+class C图片:	#平面的
+	def __init__(self, a位置: t向量3, a尺寸: t向量2, a旋转: float):
+		self.m位置 = a位置
+		self.m半尺寸 = a尺寸 * 0.5
+		self.m旋转 = a旋转	#简化计算,绕y轴旋转
+	def load(self, a路径: str):
+		v文件 = Image.open(a路径)
+		if v文件.mode != "RGBA":
+			raise ValueError("只能载入带透明通道的图片")
+		v数据 = np.array(v文件.getdata(), dtype = float)
+		v数据 /= 255.0
+		v填充 = np.zeros((v文件.width, v文件.height, 4), dtype = float)
+		for x in range(v文件.width):
+			for y in range(v文件.height):
+				y0 = v文件.height - y - 1
+				v填充[x, y0] = v数据[y0 * v文件.width + x]
+		self.m纹理 = ti.Vector.field(4, dtype = float, shape = (v文件.width, v文件.height))
+		self.m纹理.from_numpy(v填充)
+		v文件.close()
+	@ti.func
+	def hit(self, a光线, a最小值, a最大值):
+		#旋转世界,把图片放置于y-z平面
+		c = ti.cos(-self.m旋转)
+		s = ti.sin(-self.m旋转)
+		v旋转矩阵 = ti.Matrix([[c, 0.0, s], [0.0, 1.0, 0.0], [-s, 0.0, c]])
+		v相对位置 = a光线.m位置 - self.m位置
+		v相对位置 = v旋转矩阵 @ v相对位置
+		v相对方向 = v旋转矩阵 @ a光线.m方向
+		#计算相交
+		v碰撞 = False
+		t = 0.0
+		v交点 = t向量3(0.0, 0.0, 0.0)
+		v交点法线 = t向量3(0.0, 0.0, 0.0)
+		v颜色 = t向量3(0.0, 0.0, 0.0)
+		if v相对位置.x <= 0.0:	#光线在图片背面,不计算
+			pass
+		elif v相对方向.x >= 0.0:	#光线与y-z平面平行或背向y-z平面,不可能相交
+			pass
+		else:
+			t = -v相对位置.x / v相对方向.x
+			v相对交点 = v相对位置 + v相对方向 * t
+			if t < a最小值 or t > a最大值:
+				pass
+			elif abs(v相对交点.z) < self.m半尺寸.x and abs(v相对交点.y) < self.m半尺寸.y:	#交点在矩形内
+				#纹理采样
+				v纹理尺寸x = float(self.m纹理.shape[0])
+				v纹理尺寸y = float(self.m纹理.shape[1])
+				v纹理坐标x = v纹理尺寸y - (v相对交点.z / self.m半尺寸.x * 0.5 + 0.5) * v纹理尺寸x - 1	#x倒置?
+				v纹理坐标y = v纹理尺寸y - (v相对交点.y / self.m半尺寸.y * 0.5 + 0.5) * v纹理尺寸y - 1
+				v纹理坐标x0 = int(ti.floor(v纹理坐标x))
+				v纹理坐标x1 = int(ti.ceil(v纹理坐标x))
+				v纹理坐标y0 = int(ti.floor(v纹理坐标y))
+				v纹理坐标y1 = int(ti.ceil(v纹理坐标y))
+				v纹理坐标x_ = v纹理坐标x - v纹理坐标x0
+				v像素0 = lerp(self.m纹理[v纹理坐标x0, v纹理坐标y0], self.m纹理[v纹理坐标x1, v纹理坐标y0], v纹理坐标x_)
+				v像素1 = lerp(self.m纹理[v纹理坐标x0, v纹理坐标y1], self.m纹理[v纹理坐标x1, v纹理坐标y1], v纹理坐标x_)
+				v像素2 = lerp(v像素0, v像素1, v纹理坐标y - v纹理坐标y0)	#双线性纹理过滤
+				#根据像素的透明度,决定碰撞还是穿过
+				if ti.random() <= v像素2.w:
+					v碰撞 = True
+					v交点 = a光线.at(t)
+					v交点法线 = t向量3(c, 0.0, s)
+					v颜色 = t向量3(v像素2.x, v像素2.y, v像素2.z)
+		return v碰撞, t, v交点, v交点法线, True, v颜色, E材质.e漫反射
 #===============================================================================
 # 渲染
 #===============================================================================
@@ -78,7 +202,7 @@ class E材质(enum.IntEnum):
 	e漫反射 = enum.auto()
 	e镜面反射 = enum.auto()
 	e折射 = enum.auto()
-	e模糊反射 = enum.auto()
+	e模糊反射 = enum.auto()	#带点模糊的镜面反射
 @ti.data_oriented
 class C光线:
 	def __init__(self, a位置: t向量3, a方向: t向量3):
@@ -95,8 +219,7 @@ class C场景:	#存放物体
 		self.m物体.append(obj)
 	@ti.func
 	def hit(self, a光线):	#计算光与哪个物体相交,返回物体属性
-		v距离 = 10e8
-		v选择 = -1
+		v距离 = c最大值
 		v碰撞 = False
 		v交点 = t向量3(0, 0, 0)
 		v交点法线 = t向量3(0, 0, 0)
@@ -104,21 +227,17 @@ class C场景:	#存放物体
 		v颜色 = t向量3(0, 0, 0)
 		v材质 = E材质.e漫反射
 		for i in ti.static(range(len(self.m物体))):
-			x = self.m物体[i].intersect(a光线, c最小值, v距离)
-			if x > 0:
+			v碰撞0, v距离0, v交点0, v交点法线0, v前面0, v颜色0, v材质0 = self.m物体[i].hit(a光线, c最小值, v距离)
+			if v碰撞0:
 				#python似乎只把i当做整数,其它太极作用域内的变量都不是整数.直接写 self.m物体[v选择] 会报错
-				v距离 = x
-				v选择 = i
-				#计算相关变量
-				v碰撞 = True
-				v交点 = a光线.at(v距离)
-				v交点法线 = (v交点 - self.m物体[i].m位置).normalized()
-				if a光线.m方向.dot(v交点法线) < 0:
-					v前面 = True
-				else:
-					v交点法线 = -v交点法线
-				v颜色 = self.m物体[i].m颜色
-				v材质 = self.m物体[i].m材质
+				v距离 = v距离0
+				v碰撞 = v碰撞0
+				v距离 = v距离0
+				v交点 = v交点0
+				v交点法线 = v交点法线0
+				v前面 = v前面0
+				v颜色 = v颜色0
+				v材质 = v材质0
 		#返回
 		return v碰撞, v交点, v交点法线, v前面, v颜色, v材质
 @ti.data_oriented
--- 太极/载入图片.py
+++ 太极/载入图片.py
@@ -1,5 +1,7 @@
 import taichi as ti
+import time
 from PIL import Image	#pillow
+import numpy as np	#numpy
 ti.init(arch = ti.gpu)
 #画布
 c宽度 = 800
@@ -7,7 +9,7 @@ c高度 = 800
 c尺寸 = (c宽度, c高度)
 v图像 = ti.Vector.field(3, dtype = float, shape = c尺寸)
 #图片
-def init():
+def load1():	#逐像素转换
 	v文件 = Image.open("太极/logo.png")
 	v数据 = list(v文件.getdata())
 	#对齐坐标填充数据
@@ -15,14 +17,29 @@ def init():
 		for y in range(min(v文件.height, c高度)):
 			#y倒置
 			y0 = c高度 - y - 1
-			v像素 = v数据[y * v文件.width + x]
-			for i in range(3):
-				v图像[x, y0][i] = v像素[i]
+			v图像[x, y0] = ti.Vector(v数据[y * v文件.width + x]) / 255
+	v文件.close()
+def load2():	#填入numpy数组再传给场
+	v文件 = Image.open("太极/logo.png")
+	v数据 = np.array(v文件.getdata(), dtype = float)	#二维数组,shape=(像素数,通道数)
+	v数据 /= 255.0
+	v填充 = np.zeros((c宽度, c高度, 3), float)
+	#对齐坐标填充数据
+	for x in range(min(v文件.width, c宽度)):
+		for y in range(min(v文件.height, c高度)):
+			#y倒置
+			y0 = c高度 - y - 1
+			v填充[x, y0] = v数据[y * v文件.width + x]
+	v图像.from_numpy(v填充)
 	v文件.close()
 #窗口
 v窗口 = ti.ui.Window("载入图片", res = c尺寸)
 v画布 = v窗口.get_canvas()
-init()
+v开始时间 = time.time()
+# load1()
+load2()
+v结束时间 = time.time()
+print("耗时:", v结束时间 - v开始时间)
 while v窗口.running:
 	v画布.set_image(v图像)
 	v窗口.show()
